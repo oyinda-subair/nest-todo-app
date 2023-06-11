1ab90f5e88514866b172f8bb43e5776f13cb7719
oyinda-subair <oyinda.subair@gmail.com>
2023-06-11 04:10:22 +0100

    configure typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoController } from './todo/todo.controller';
 import { TodoService } from './todo/todo.service';
 import { TodoModule } from './todo/todo.module';
+import { getEnvPath } from './common/helper/env.helper';
+import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
 
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 @Module({
-  imports: [TodoModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+    TodoModule,
+  ],
   controllers: [AppController, TodoController],
   providers: [AppService, TodoService],
 })
