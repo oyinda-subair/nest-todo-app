13a5636ae91731ebb0c7caa2310632daaf0d6ad1
oyinda-subair <oyinda.subair@gmail.com>
2023-06-19 19:50:47 +0100

    update imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,21 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodoController } from './todo/todo.controller';
-import { TodoService } from './todo/todo.service';
-import { TodoModule } from './todo/todo.module';
+import { TodoController } from './api/todo/todo.controller';
+import { TodoService } from './api/todo/todo.service';
+import { TodoModule } from './api/todo/todo.module';
 import { getEnvPath } from './common/helper/env.helper';
+import { validate } from './common/helper/env.validation';
 import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
+import { ApiModule } from './api/api.module';
 
 const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    ConfigModule.forRoot({ envFilePath, isGlobal: true, validate }),
     TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
     TodoModule,
+    ApiModule,
   ],
   controllers: [AppController, TodoController],
   providers: [AppService, TodoService],
