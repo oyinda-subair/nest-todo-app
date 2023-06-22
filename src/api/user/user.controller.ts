05da1c05f79452dc8d52a50587efa44d774666e5
oyinda-subair <oyinda.subair@gmail.com>
2023-06-22 01:27:49 +0100

    Add new db function to update, delete and find all

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,22 +1,28 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Inject,
+  NotFoundException,
   Param,
   ParseIntPipe,
   Post,
+  Put,
 } from '@nestjs/common';
-import { CreateUserDto } from './user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './user.entity';
 import { UserService } from './user.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
-  @Inject(UserService)
-  private readonly service: UserService;
+  constructor(
+    @Inject(UserService)
+    private readonly service: UserService,
+  ) {}
 
-  @Get(':id')
+  @Get('/:id')
   public getUser(@Param('id', ParseIntPipe) id: number): Promise<User> {
     return this.service.getUser(id);
   }
@@ -24,5 +30,28 @@ export class UserController {
   @Post()
   public createUser(@Body() body: CreateUserDto): Promise<User> {
     return this.service.createUser(body);
+  }
+
+  @Get()
+  public getAllUsers(): Promise<User[]> {
+    return this.service.findAll();
+  }
+
+  @Put('/:id')
+  async updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() user: UpdateUserDto,
+  ) {
+    return this.service.update(id, user);
+  }
+
+  @Delete(':id')
+  async deleteTodo(@Param('id') id: number) {
+    const user = await this.service.getUser(id);
+    if (!user) {
+      throw new NotFoundException('User does not exist!');
+    }
+
+    return this.service.delete(id);
   }
 }
